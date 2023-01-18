ff87883433bbe410fc36ad51d8311c9567d3b087
itsmeleonardo <carlosgamercovi20@gmail.com>
2023-01-18 16:29:20 -0500

    complete duolingo keyboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
-import { useState } from "react";
+import { useEffect } from "react";
+import { useHookstate, none } from "@hookstate/core";
+
 import "./App.css";
 import WordBtn from "./WordBtn";
 
@@ -6,46 +8,118 @@ const words = [
   "apple",
   "banana",
   "cherry",
-  // "dragonfruit",
+  "dragonfruit",
   "elderberry",
-  "appluu",
+  "pineapple",
+  "watermelon",
+  "carrot",
 ];
 
+export type WordState = {
+  selectedWords: string[];
+  words: string;
+};
+
+const initialState: WordState = {
+  selectedWords: [],
+  words: "",
+};
+
 function App() {
-  const [selected, setSelected] = useState<string[]>([]);
+  const state = useHookstate<WordState>(initialState);
 
   const handleAddWord = (word: string) => {
-    setSelected((prev) => [...prev, word]);
+    const exist = state.selectedWords.value.includes(word);
+    if (exist) return;
+    state.selectedWords.merge([word]);
+    state.words.set("");
   };
 
   const handleRemoveWord = (word: string) => {
-    setSelected((prev) => prev.filter((w) => w !== word));
+    const index = state.selectedWords.value.indexOf(word);
+    if (index === -1) return;
+    state.selectedWords.merge({ [index]: none });
   };
 
   const getWordSelected = (word: string) => {
-    return selected.includes(word);
+    return state.selectedWords.value.includes(word);
   };
+
+  const removeLastWord = () => {
+    const index = state.selectedWords.value.length - 1;
+    if (index === -1) return;
+    state.selectedWords.merge({ [index]: none });
+  };
+
+  useEffect(() => {
+    const getWordMatches = (letter: string) => {
+      const matches = words
+        .filter((word) => !state.selectedWords.value.includes(word))
+        .filter((word) => word.startsWith(letter));
+      return matches;
+    };
+
+    const handleBackspace = () => {
+      if (state.words.value === "") {
+        removeLastWord();
+        return;
+      }
+      state.words.set((prev) => prev.slice(0, -1));
+    };
+
+    const handleKeyDown = (e: KeyboardEvent) => {
+      const { key } = e;
+      if (key === "Backspace") {
+        handleBackspace();
+        return;
+      }
+      if (key < "a" || key > "z") return;
+
+      const matches = getWordMatches(`${state.words.value}${key}`);
+
+      if (matches.length === 0) {
+        return;
+      }
+
+      if (matches.length === 1) {
+        const [word] = matches;
+        handleAddWord(word);
+        return;
+      }
+
+      state.words.set((prev) => prev + key);
+    };
+
+    window.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, []);
 
   return (
     <div className="container flex flex-col gap-4 mx-auto">
       <div className="w-full border border-dashed border-indigo-600 max-w-[400px] mx-auto h-[40px] flex gap-3 items-center justify-center">
-        {selected.map((word) => (
+        {state.selectedWords.map((word) => (
           <button
+            key={word.value}
             className="text-indigo-600"
-            onClick={() => handleRemoveWord(word)}
+            onClick={() => handleRemoveWord(word.value)}
           >
-            {word}
+            {word.value}
           </button>
         ))}
       </div>
 
-      <div className="w-full  max-w-[400px] mx-auto flex gap-2">
+      <div className="w-full  max-w-[400px] mx-auto flex flex-wrap gap-2">
         {words.map((word) => (
           <WordBtn
+            key={word}
             word={word}
             onMatch={handleAddWord}
             onSelected={handleAddWord}
             selected={getWordSelected(word)}
+            wordState={state}
           />
         ))}
       </div>
--- src/WordBtn.tsx
+++ src/WordBtn.tsx
@@ -1,52 +1,36 @@
 import { useEffect, useState } from "react";
+import { State } from "@hookstate/core";
+
+import type { WordState } from "./App";
 
 type Props = {
   word: string;
   onMatch: (word: string) => void;
   onSelected: (word: string) => void;
   selected?: boolean;
+  wordState: State<WordState>;
 };
 
-export default function WordBtn({
-  word,
-  onMatch,
-  selected,
-  onSelected,
-}: Props) {
-  const [wordSelected, setWordSelected] = useState("");
+export default function WordBtn(props: Props) {
+  const { word, onMatch, selected, onSelected, wordState } = props;
 
   useEffect(() => {
-    const handleKeyDown = (e: KeyboardEvent) => {
-      const { key } = e;
-      if (key < "a" || key > "z") return;
-
-      setWordSelected((prev) => prev + key);
-    };
-
-    window.addEventListener("keydown", handleKeyDown);
-
-    return () => {
-      window.removeEventListener("keydown", handleKeyDown);
-    };
-  }, []);
-
-  useEffect(() => {
-    if (wordSelected === word) {
+    if (wordState.words.value === word) {
       onMatch(word);
-      setWordSelected("");
     }
-  }, [wordSelected]);
+  }, [wordState.words.value]);
 
   return (
     <button
-      className="px-2 py-1 font-bold bg-slate-200 disabled:opacity-10"
+      className="px-2 py-1 font-bold bg-slate-200 disabled:opacity-10 rounded"
       disabled={selected}
       onClick={() => onSelected(word)}
     >
       {[...word].map((letter, index) => (
         <span
+          key={index}
           className={`text-slate-600 ${
-            wordSelected[index] === letter ? "text-purple-500" : ""
+            wordState.words.value[index] === letter ? "text-purple-500" : ""
           }`}
         >
           {letter}
